e6a4a67bb48616cb8f94e7f54da91ab66694f2bb
KonradKamilChwalowski <ace55@wp.pl>
2025-07-04 23:40:28 +0200

    Add EasterEgg with logo

--- fiszki-prot-2/scripts/menu_screen.gd
+++ fiszki-prot-2/scripts/menu_screen.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
 @onready var game_manager := get_parent()
+@onready var logo_texture: TextureRect = $Logo
+
+var is_rotating: bool = false
+var easter_egg_counter: int = 0
+var rotation_elapsed: float = 0.0
+
+func _process(delta: float) -> void:
+	# ROTATION
+	if is_rotating:
+		rotation_elapsed += delta
+		var t = rotation_elapsed / (0.4)
+		if t <= 1.0:
+			logo_texture.scale.x = lerp(0.65, 0.0, t)
+		elif t <= 2.0:
+			logo_texture.texture = load("res://assets/Logo_face.png")
+			var t2 = t - 1.
+			logo_texture.scale.x = lerp(0.0, 0.65, t2)
+		else:
+			is_rotating = false
 
 func _on_start_button_pressed() -> void:
 	game_manager.change_screen("language_choice_screen")
@@ -13,3 +32,11 @@ func _on_options_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_easter_egg_button_pressed() -> void:
+	# The flashcard is rotating in _process, so here we just start proces
+	easter_egg_counter += 1
+	if not is_rotating and easter_egg_counter == 5:
+		is_rotating = true
+		rotation_elapsed = 0.0
